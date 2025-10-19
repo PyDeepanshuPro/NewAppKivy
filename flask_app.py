e2edcc3160387f972c89acc06f6336bd9cfc07fb
PyDeepanshuPro <deepanshuc506@gmail.com>
2025-10-19 07:22:28 +0530

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'नमस्ते, यह मेरी पहली फ्लास्क वेबसाइट है!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
